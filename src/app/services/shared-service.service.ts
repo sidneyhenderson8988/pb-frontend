8bbf3dbb19ccaa72dac95d9bc0ececb247e79894
Sidney Henderson <shende39@uncc.edu>
2020-12-15 12:31:09 -0500

    first commit for frontend

---
+++ src/app/services/shared-service.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SharedServiceService {
+  sharedSubject: BehaviorSubject<string> = new BehaviorSubject<string>('guest');
+  constructor() {}
+}
